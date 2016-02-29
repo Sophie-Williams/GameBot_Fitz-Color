9a1371069be32b5d25f64dc7bd37bfa4fb67524f
amitsaroj001 <amitsaroj2012@gmail.com>
2016-02-29 12:27:11 -0800

    remove bug

--- fitz.py
+++ fitz.py
@@ -1,9 +1,13 @@
 """ 
 All coordinates assume a screen resolution of 1280x1024, and Chrome 
 maximized with the Bookmarks Toolbar enabled.
+with Task bar at the bottom
+Operating System: Windows 10
 x_pad = 459
 y_pad = 100
 Play area =  x_pad+1, y_pad+1, 904, 767
+Precaution: don't move cursor while bot is running,
+bot might get disturbed :P
 """
 
 from PIL import ImageGrab
@@ -12,20 +16,22 @@ import time
 from PIL import ImageOps
 from numpy import *
 import win32api, win32con
-from Tkinter import *
+import Tkinter as tk
 
-if not os.path.exists(os.getcwd() + '\\images'):    
-    os.makedirs(os.getcwd() + '\\images')
+# Global
+# --------
+x_pad = 459
+y_pad = 91
 
-def snapShot():     
+def snapShot():
     box = (x_pad+1, y_pad+1, x_pad+445, y_pad+667)
     im = ImageGrab.grab(box)
     im = ImageGrab.grab(box)
-    im.save(os.getcwd() + '\\images\\actual_snap_' + str(int(time.time())) + '.png', 'PNG')
+    im.save(os.getcwd() + '\\actual_snap_' + str(int(time.time())) + '.png', 'PNG')
 
 def leftClick():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(.99)
+    time.sleep(.095)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
     print 'CLICK'
 
@@ -38,28 +44,47 @@ def get_cords():
     y = y - y_pad
     print x,y
 
-class Cord:                 
-    play = (229, 250)
-    first_box = (222, 269)
-    second_box = (222, 412)
-    third_box = (222, 554)
+color_text = {
+    34596: 'green',
+    33348: 'gray',
+    32333: 'blue',
+    33368: 'purple',
+    35696: 'orange',
+    30032: 'red'
+    }
+
+color_color = {
+    'green':8579,
+    'gray':8609,
+    'blue':8572,
+    'purple':8568,
+    'orange':8593,
+    'red':8536
+    }
+
+class Cord:
     
+    first_box = (222, 240)
+    second_box = (222, 380)
+    third_box = (222, 524)
+    
+
 def titleColor():
-    box = (x_pad+149, y_pad+96, x_pad+251, y_pad+183)
+    box = (x_pad+109, y_pad+87, x_pad+199, y_pad+153)
     im = ImageOps.grayscale(ImageGrab.grab(box))
     a = array(im.getcolors())
     a = a.sum()
     print a
-    # im.save(os.getcwd() + '\\image\\title_snap' + str(int(time.time())) + '.png', 'PNG')
+    # im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
     return a
 
 def colorBox_1():
-    box = (x_pad+160, y_pad+240, x_pad+300, y_pad+300)
+    box = (x_pad+160, y_pad+220, x_pad+300, y_pad+280)
     im = ImageOps.grayscale(ImageGrab.grab(box))
     a = array(im.getcolors())
     a = a.sum()
-    # print a
-    # im.save(os.getcwd() + '\\image\\box_1_snap' + str(int(time.time())) + '.png', 'PNG')     # for debugging
+    print a
+    # im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
     return a
 
 def colorBox_2():
@@ -67,17 +92,17 @@ def colorBox_2():
     im = ImageOps.grayscale(ImageGrab.grab(box))
     a = array(im.getcolors())
     a = a.sum()
-    # print a
-    # im.save(os.getcwd() + '\\image\\box_2_snap' + str(int(time.time())) + '.png', 'PNG')     # for debugging
+    print a
+    # im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
     return a
 
 def colorBox_3():
-    box = (x_pad+160, y_pad+520, x_pad+300, y_pad+580)
+    box = (x_pad+160, y_pad+510, x_pad+300, y_pad+570)
     im = ImageOps.grayscale(ImageGrab.grab(box))
     a = array(im.getcolors())
     a = a.sum()
-    # print a
-    # im.save(os.getcwd() + '\\box_3_snap' + str(int(time.time())) + '.png', 'PNG')     # for debugging
+    print a
+    # im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
     return a
 
 def Go():
@@ -108,50 +133,9 @@ def Go():
         #time.sleep(.1)
         return
 
-def gui():
-    gui_d = Tk()
-    gui_d.geometry('200x100+800+400')
-    gui_d.title("Fitz-Color Bot")
-    gui_d.mainloop()
-    label_1 = Label(gui_d,text = 'Enter the Score').pack()
-    button_1 = Button(gui_d, text = "OK", command = pass_text()).pack()
-
-    def pass_text():
-        return label_1.text
-
-    
 
 def start():
-    for x in range(0,130):
+    for x in range(0,int(100)):
         Go()
-        time.sleep(.019)
-#---------------------------------------------------------------------------------
-        
-# Global
-# --------
-x_pad = 459
-y_pad = 100
+        time.sleep(.022)
 
-color_text = {
-    37950: 'green',
-    38061: 'gray',
-    37380: 'blue',
-    37311: 'purple',
-    37783: 'orange',
-    38545: 'red'
-    }
-
-color_color = {
-    'green':8579,
-    'gray':8609,
-    'blue':8572,
-    'purple':8568,
-    'orange':8593,
-    'red':8536
-    }
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
---
+++ screenshot.py
@@ -0,0 +1,62 @@
+
+from PIL import ImageGrab
+import os
+import time
+from PIL import ImageOps
+from numpy import *
+
+# global
+#==========
+x_pad = 467
+y_pad = 84
+
+if not os.path.exists(os.getcwd() + '\\images'):    
+    os.makedirs(os.getcwd() + '\\images')
+
+def snapShot():     # it was used for setting the co-ordinate of the game
+    box = (x_pad+1, y_pad+1, x_pad+429, y_pad+640)
+    im = ImageGrab.grab(box)
+    im = ImageGrab.grab(box)
+    im.save(os.getcwd() + '\\images\\actual_snap_' + str(int(time.time())) + '.png', 'PNG')
+
+def titleColor():
+    box = (x_pad+101, y_pad+94, x_pad+191, y_pad+160)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print a
+    im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
+    return a
+
+def colorBox_1():
+    box = (x_pad+160, y_pad+220, x_pad+300, y_pad+280)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print a
+    im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
+    return a
+
+def colorBox_2():
+    box = (x_pad+160, y_pad+370, x_pad+300, y_pad+430)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print a
+    im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
+    return a
+
+def colorBox_3():
+    box = (x_pad+160, y_pad+510, x_pad+300, y_pad+570)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print a
+    im.save(os.getcwd() + '\\actual_snap' + str(int(time.time())) + '.png', 'PNG')
+    return a
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
